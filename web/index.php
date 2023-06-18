76946d69111ad6905e32ea97a5cd8353344e2bbc
Deni Gentar Candana <136276001+satoribyte@users.noreply.github.com>
2023-06-18 16:18:49 +0700

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center mt-5">Hello, World!</h1>
+
+        <div class="card mt-5">
+            <div class="card-body">
+                <h2 class="card-title text-center">Puisi Cinta</h2>
+                <p class="card-text">
+                    Aku mencintaimu dengan segenap hatiku<br>
+                    Setiap detik, setiap nafasku hanya untukmu<br>
+                    Kau bagian dari hidupku, kau jiwaku<br>
+                    Kita bersama, melewati liku-liku<br>
+                    Cinta ini tak pernah pudar, tak pernah surut<br>
+                    Seperti ombak yang terus menghantam pantai<br>
+                    Kita bersama, saling melengkapi, saling membutuhkan<br>
+                    Cinta kita bagaikan bintang di angkasa<br>
+                    Bersinar terang, menghiasi langit malam<br>
+                    Kau dan aku, tak terpisahkan<br>
+                    Dalam cinta ini, kita bersatu selamanya
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
